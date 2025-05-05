aa45e920f2d0915157fafacc1a5710b1238d57fc
Maria James <mariajamesv22@gmail.com>
2025-05-06 00:33:40 +0530

    create public and private route table and associate subnets with route tables

--- Session_2/network.tf
+++ Session_2/network.tf
@@ -91,3 +91,54 @@ resource "aws_nat_gateway" "main" {
     Name = format("%s-nat-gateway", var.prefix)
   }
 }
+
+# Create Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = format("%s-public-route-table", var.prefix)
+  }
+}
+
+# Create Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = format("%s-private-route-table", var.prefix)
+  }
+}
+
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
