6b43de5d364f421495925d6f4d60e510c0a9cf46
Maria James <mariajamesv22@gmail.com>
2025-05-06 00:26:19 +0530

    create internet gatway, nat gateway and EIP

--- Session_2/network.tf
+++ Session_2/network.tf
@@ -66,3 +66,28 @@ resource "aws_subnet" "secure_2" {
     Name = format("%s-secure-subnet-1", var.prefix)
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = format("%s-internet-gateway", var.prefix)
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = format("%s-eip-nat", var.prefix)
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = format("%s-nat-gateway", var.prefix)
+  }
+}
