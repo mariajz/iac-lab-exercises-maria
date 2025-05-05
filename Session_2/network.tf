3204b4df92a3abf8a5c49af622c7def64ff785fd
Maria James <mariajamesv22@gmail.com>
2025-05-06 00:11:37 +0530

    create subnets

--- Session_2/network.tf
+++ Session_2/network.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
@@ -6,5 +10,59 @@ resource "aws_vpc" "main" {
 
   tags = {
     Name = "${var.prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet1_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = format("%s-public-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet2_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = format("%s-public-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet3_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = format("%s-private-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet4_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = format("%s-private-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet5_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = format("%s-secure-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet6_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = format("%s-secure-subnet-1", var.prefix)
   }
 }
